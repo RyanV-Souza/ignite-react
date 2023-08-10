aa12e67a8c490a5c730267cae587da811d1440e5
Ryan Vieira <ryan.vieira@gbmlogistica.com.br>
2023-08-10 19:36:25 -0300

    Componente: Post

---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,36 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://images.unsplash.com/photo-1691443297137-68818fe7bce9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=40"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Ryan Vieira</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="10 de Agosto às 19:30h" dateTime="2023-08-10 19:30:00">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href=""> jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
