07bd3ba8def1a67fb81c7ebb10a2fe22e5ada692
Ryan Vieira <ryan.vieira@gbmlogistica.com.br>
2023-08-22 22:27:04 -0300

    Removendo comentários (imutabilidade)

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -34,8 +34,12 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value)
   }
 
-  function deleteComment(comment) {
-    console.log('deletar', comment)
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(
+      (comment) => comment !== commentToDelete
+    )
+
+    setComments(commentsWithoutDeletedOne)
   }
 
   return (
